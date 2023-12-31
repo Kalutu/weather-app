3607d8e670dd064d650d94b2601260a8504c772c
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-12-31 17:26:20 +0300

    Get user location

--- App.js
+++ App.js
@@ -1,13 +1,49 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import Tabs from "./src/components/Tabs";
+import React, { useState, useEffect } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import Tabs from './src/components/Tabs'
+import { ActivityIndicator, View, StyleSheet } from 'react-native'
+import * as Location from 'expo-location'
+
+//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 const App = () => {
+  const [loading, setLoading] = useState(true)
+  const [location, setLocation] = useState(null)
+  const [error, setError] = useState(null)
+  useEffect(() => {
+    ;(async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted') {
+        setError('Permission to access location was denied')
+        return
+      }
+      let location = await Location.getCurrentPositionAsync({})
+      setLocation(location)
+    })()
+  }, [])
+
+  if (location) {
+    console.log(location)
+  }
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size={'large'} color={'blue'} />
+      </View>
+    )
+  }
   return (
     <NavigationContainer>
       <Tabs />
     </NavigationContainer>
-  );
-};
+  )
+}
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    flex: 1,
+  },
+})
+export default App
